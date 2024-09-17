67a88712ce26c2bcc3d53b4e3419a0663c8981cc
Roger Tavares <rogertavaresoperacional@gmail.com>
2024-09-17 20:52:08 -0300

    Variáveis tipos de dados 01

---
+++ Aula010_Variáveis_Tipos de dados/src/curso/Variáveis_02.java
@@ -0,0 +1,21 @@
+package curso;
+
+/**
+ * 
+ * @author roger
+
+ * programa para efetuar a soma de 5 + 3
+ * 
+ */
+public class Variáveis_02 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println (5 + 3);
+		
+		
+		
+		
+	}
+
+}
---
+++ Aula010_Variáveis_Tipos de dados/src/curso/variáveis_03.java
@@ -0,0 +1,28 @@
+package curso;
+
+/**
+ * @author roger
+ * Declaração de variáveis
+ * 
+ */
+public class variáveis_03 {
+	
+	public static void main(String[] args) {
+		
+		//declaração de variáveis
+		//<tipo> <nome da variável>;
+		//<tipo> <nome da variável> = <valor;
+		//tipo inteiro
+		int a; //declaração da variável
+		a = 5; //atribuição do valor à variavel
+		//declarando e inicializando a variável
+		int b = 3;
+		
+		System.out.println (a + b);
+		//declarar variável que vai ter a soma de a + b
+		int soma = a + b;
+		System.out.println(soma);
+		
+	}
+
+}
